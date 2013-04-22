49a11b7b94627e42fdafc573619136524ce1c602
jshou <joshuahou@gmail.com>
2013-04-21 23:17:17 -0700

    my solution

--- round1/1.c
+++ round1/1.c
@@ -3,26 +3,15 @@
 
 void main() {
   gchar *hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+  int hexLength = strlen(hexString);
+  char *inp = (char*) malloc(hexLength/2);
 
-  int i, c = strlen(hexString);
+  gchar *phexString = hexString;
 
-  char *inp = (char*)malloc(c / 2);
-
-  char *p1 = hexString;
-  char *p2 = inp;
-
-  for (i=0; i<c; i++) {
-    *p2 = *p1<='9' ? *p1 - '0' : (*p1 + 10) - 'a';
-    *p2 <<= 4;
-
-    p1++;
-
-    *p2 += *p1<='9' ? *p1 - '0' : (*p1 + 10) - 'a';
-
-    p1++;
-    p2++;
+  for (int i = 0; i < hexLength; i++) {
+    sscanf(phexString, "%2x", &inp[i]);
+    phexString += 2 * sizeof(char);
   }
-  *p2 = 0;
 
   gchar *output = g_base64_encode(inp, strlen(hexString)/2);
 
