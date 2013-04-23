2d0c185fd4785716e746ced4f81511fa31138a66
jshou <joshuahou@gmail.com>
2013-04-22 18:50:46 -0700

    terminate output strings, only iterate over length/2 for hexToByte so as not to overwrite

--- round1/converter.c
+++ round1/converter.c
@@ -3,10 +3,12 @@
 void hexToByte(char* hexString, char* byteString) {
   int length = strlen(hexString);
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length/2; i++) {
     sscanf(hexString, "%2x", (unsigned int*) &byteString[i]);
     hexString += 2 * sizeof(char);
   }
+
+  byteString[length] = 0;
 }
 
 void byteToHex(char* byteString, char* hexString) {
@@ -15,4 +17,6 @@ void byteToHex(char* byteString, char* hexString) {
     sprintf(&hexString[i], "%2x", (unsigned int) *byteString);
     byteString++;
   }
+
+  hexString[length] = 0;
 }
