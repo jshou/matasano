17157a93c7db4b2ef4a6862867a09a946176c3a7
jshou <joshuahou@gmail.com>
2013-04-23 23:06:21 -0700

    iterate over hexstring, don't need to calculate length of hex string

--- round1/converter.c
+++ round1/converter.c
@@ -1,14 +1,11 @@
 #include "converter.h"
 
 void hexToByte(char* hexString, char* byteString) {
-  int length = strlen(hexString);
-
-  for (int i = 0; i < length/2; i++) {
-    sscanf(hexString, "%2x", (unsigned int*) &byteString[i]);
-    hexString += 2 * sizeof(char);
+  while (*hexString) {
+    sscanf(hexString, "%2x", (unsigned int*) byteString);
+    hexString += 2;
+    byteString++;
   }
-
-  byteString[length] = 0;
 }
 
 void byteToHex(char* byteString, char* hexString, int byteLength) {
