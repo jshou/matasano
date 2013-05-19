1e71e6933386dbcacad9ff8312629e7c3f341f6b
jshou <joshuahou@gmail.com>
2013-05-19 16:19:26 -0700

    add method for variable xor key length

--- round1/xor.c
+++ round1/xor.c
@@ -1,7 +1,11 @@
 #include "xor.h"
 
 void xor_decode(char *ciphertext, char *plaintext, int length, char key) {
+  xor(ciphertext, plaintext, length, &key, 1);
+}
+
+void xor(char *ciphertext, char *plaintext, int length, char *key, int key_length) {
   for(int i = 0; i < length; i++) {
-    plaintext[i] = ciphertext[i] ^ key;
+    plaintext[i] = ciphertext[i] ^ key[i % key_length];
   }
 }
--- round1/xor.h
+++ round1/xor.h
@@ -1,1 +1,2 @@
 void xor_decode(char *ciphertext, char *message, int length, char key);
+void xor(char *ciphertext, char *plaintext, int length, char *key, int key_length);
