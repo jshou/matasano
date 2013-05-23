cf3080a998e64b1f22c2f59ae7c81456bc3a8a80
jshou <joshuahou@gmail.com>
2013-05-22 23:16:59 -0700

    add function for calculating hamming distance

--- round1/xor.c
+++ round1/xor.c
@@ -5,3 +5,15 @@ void xor_decode(char *ciphertext, char *plaintext, int length, char *key, int ke
     plaintext[i] = ciphertext[i] ^ key[i % key_length];
   }
 }
+
+int hamming_distance(char *a, char*b, int length) {
+  int distance = 0;
+
+  for(int i = 0; i < length; i++) {
+    if (a[i] != b[i]) {
+      distance++;
+    }
+  }
+
+  return distance;
+}
--- round1/xor.h
+++ round1/xor.h
@@ -1,1 +1,2 @@
 void xor_decode(char *ciphertext, char *message, int length, char *key, int key_length);
+int hamming_distance(char *a, char*b, int length);
