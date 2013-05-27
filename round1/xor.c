f47421b49d6ca3da544cb79984b83230ca6f480c
jshou <joshuahou@gmail.com>
2013-05-26 21:18:42 -0700

    extract function to figure out best 1-char key

--- round1/xor.c
+++ round1/xor.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include "xor.h"
 
 void xor_decode(char *ciphertext, char *plaintext, int length, char *key, int key_length) {
@@ -16,4 +18,30 @@ int hamming_distance(char *a, char*b, int length) {
   }
 
   return distance;
+}
+
+char xor_best_key(char *ciphertext,
+                  char *message,
+                  int length,
+                  float (*eval_func)(char *msg, int length)) {
+  char bestKey;
+  float bestScore;
+  char *currentMessage = malloc(length);
+
+  for(int key = 0; key < 256; key++) {
+    char charkey = (char) key;
+    xor_decode(ciphertext, currentMessage, length, &charkey, 1);
+
+    float currentScore = eval_func(currentMessage, length);
+
+    if (currentScore >= bestScore) {
+      bestKey = key;
+      bestScore = currentScore;
+      strcpy(message, currentMessage);
+    }
+  }
+
+  free(currentMessage);
+
+  return bestKey;
 }
--- round1/xor.h
+++ round1/xor.h
@@ -1,2 +1,6 @@
 void xor_decode(char *ciphertext, char *message, int length, char *key, int key_length);
 int hamming_distance(char *a, char*b, int length);
+char xor_best_key(char *ciphertext,
+                  char *message,
+                  int length,
+                  float (*eval_func)(char *msg, int length));
